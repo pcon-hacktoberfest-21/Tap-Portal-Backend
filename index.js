f54c36b5883c3ad9f957674bccfc5864eb2102ad
amit-k-umar <61649790+amit-k-umar@users.noreply.github.com>
2021-01-30 13:28:34 +0530

    make logger

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ var db= require('./db')
 
 
 
-// only for testiong
-//var logger = require('./utils/logger')
-// app.get('/',(req,res)=>{
-//     var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
-//    logger("user loged in","2029UGCS999",ip).then(res.send("hello"))
-   
-// })
+
 
 app.listen(PORT,()=>{
     console.log(`Listening at post ${PORT}`)
