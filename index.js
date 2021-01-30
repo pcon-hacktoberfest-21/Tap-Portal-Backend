3ea0589d14e4b73654354b0be14d72a467767f31
amit-k-umar <61649790+amit-k-umar@users.noreply.github.com>
2021-01-30 15:38:43 +0530

    Logger
    :notebook: Logger and database connection added

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT
 const express = require("express")
 const app = express()
 
+
+// importing database content
+var db= require('./db')
+
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`Listening at post ${PORT}`)
 })
