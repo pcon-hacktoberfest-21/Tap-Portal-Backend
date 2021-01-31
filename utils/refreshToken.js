2face99d16a3cb1e8c5068a85ed6c9fae556b27d
Aman Jha <aman.jha21.aj@gmail.com>
2021-01-31 18:23:07 +0545

    admin routes

--- utils/refreshToken.js
+++ utils/refreshToken.js
@@ -4,17 +4,25 @@ const jwt = require('jsonwebtoken');
 
 const refreshToken = (token, email) => {
     try{
+
+        //Valid token
         const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
         console.log('decoded',decoded)
         return token;
+        
     }
     catch(err){
+
+        //expired token
         console.log('error',err)
+
+        //issuing new token
         token = jwt.sign({
             Email: email,
         },process.env.TOKEN_SECRET,{expiresIn: '1h',});
         console.log(token)
-        return token
+        return token;
+
     }
 }
 
