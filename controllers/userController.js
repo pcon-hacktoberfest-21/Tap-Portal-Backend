3eb4c0cdd36b76322065f83f75bfa40d9e8c2302
Aman Jha <aman.jha21.aj@gmail.com>
2021-02-02 10:00:24 +0545

    vadidation

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,9 +7,10 @@ const { validateStudent } = require('../middleWares/validation');
 module.exports = {
   //Login USERS
   login: async (req, res) => {
-    //Acquiring email and passwords from body
-    const email = req.body.email; /*2019ugcs001@nitjsr.ac.in*/
-    const password = req.body.password; /*'password'*/
+
+      //Acquiring email and passwords from body
+      const email = req.body.email;  
+      const password = req.body.password;   
 
     //INPUT DATA VALIDATION
     const validation = validateStudent(req);
@@ -19,94 +20,88 @@ module.exports = {
         .send({ status:false,message: validation.error.details[0].message });
     }
 
-    //SQL Query
-    db.query(
-      `SELECT * FROM ALL_USER WHERE Email = ? `,
-      [email],
-      async function (err, results) {
-        if (err) {
-          console.log(err);
-          res.status(500).send({
-            message: "Something went wrong",
-          });
-        } else {
-          let status = true,
-            message;
-          //USER is found
-          if (results.length > 0) {
-            const isEqual = await bcrypt.compare(password, results[0].Password);
-            if (!isEqual) {
-              //Incorrect Password
-              console.log("Incorrect Password");
-              status = false;
-              message = "Incorrect Password";
-            }
-            let token;
-
-            //If token is not there then providing a new token
-            if (results[0].Token == null) {
-              token = jwt.sign(
-                {
-                  Email: results[0].Email,
-                },
-                process.env.TOKEN_SECRET,
-                { expiresIn: "1h" }
-              );
-            }
-
-            //If token is already there then checking for its expiry
-            //If expired providing him/ her new token
-            //Else returing the same token
-            else {
-              token = refreshToken(results[0].Token, results[0].Email);
-            }
-
-            console.log(token);
-
-            //Saving the token in database
-            db.query(
-              `UPDATE ALL_USER SET Token = ? WHERE Email = ?`,
-              [token, email],
-              function (err, results) {
-                if (err) {
-                  console.log(err);
-                  status = false;
-                  message = "Something went wrong";
-                } else {
-                  console.log(results);
-                }
-              }
-            );
-
-            if (status) {
-              res.send({
-                status,
-                token,
-                email: results[0].Email,
-              });
-            } else {
-              res.send({
-                status,
-                message,
-              });
-            }
-          } else {
-            console.log("Student not found");
-            res.status(400).send({
-              status:false,
-              message: "Student not found",
-            });
+        //SQL Query
+        db.query(`SELECT * FROM ALL_USER WHERE Email = ? `,[email],async function(err, results){
+          if(err){
+             console.log(err)
+             res.send({
+                  status: false,
+                  message:"Something went wrong"
+             })
           }
-        }
-      }
-    );
-  },
+          else  {
+              let status = true, message;
+              //USER is found
+              if(results.length > 0){
+                  const isEqual = await bcrypt.compare(password,results[0].Password);
+                  if(!isEqual){
+                      //Incorrect Password 
+                      console.log( "Incorrect Password");
+                      status = false;
+                      message = "Incorrect Password";
+                  }
 
-  // Register USERS
-  register: async (req, res) => {
-    //Requiring email and password from body
-    const email = req.body.email; /*2019ugcs001@nitjsr.ac.in*/
-    const password = req.body.password; /*'password'*/
+                  let token;
+
+                  //If token is not there then providing a new token
+                  if(results[0].Token == null){
+                      token = jwt.sign({
+                          Email: results[0].Email,
+                      },process.env.TOKEN_SECRET,{expiresIn: '1h',});
+                  }
+
+                  //If token is already there then checking for its expiry 
+                  //If expired providing him/ her new token
+                  //Else returing the same token 
+                  else{
+                      token = refreshToken(results[0].Token, results[0].Email);
+                  }
+
+                  console.log(token)
+
+                  //Saving the token in database
+                  db.query(`UPDATE ALL_USER SET Token = ? WHERE Email = ?`,[token, email], function(err, results){
+                      if(err){
+                          console.log(err)
+                          status = false;
+                          message = "Something went wrong";
+                      }
+                      else{
+                          console.log(results)
+                      }
+                  })
+
+                  if(status){
+                      res.send({
+                          status,
+                          token,
+                          email:  results[0].Email
+                      })
+                  }
+                  else{
+                      res.send({
+                          status,
+                          message
+                      })
+                  }
+              }
+              else{
+                  console.log("Student not found")
+            res.status(400).send({
+              status: false,
+              message: "Student not found"
+          })
+      }
+  }
+})
+}, 
+
+// Register USERS
+register: async (req, res) => {
+
+//Requiring email and password from body
+const email = req.body.email;
+const password = req.body.password; 
 
     //INPUT DATA VALIDATION
     const validation = validateStudent(req);
@@ -116,36 +111,33 @@ module.exports = {
         .send({ status:false,message: validation.error.details[0].message });
     }
 
-    //Hashing the password
-    const hashedPassword = await bcrypt.hash(password, 12);
+        //Hashing the password
+        const hashedPassword = await bcrypt.hash(password,12)
 
-    //SQL query
-    db.query(
-      `INSERT INTO ALL_USER (Email, Password) VALUES(?,?)`,
-      [email, hashedPassword],
-      function (err, results) {
-        //Registration failed
-        if (err) {
-          console.log(err);
-          res.send({
-            status: false,
-            message: err.sqlMessage,
-          });
+        //SQL query
+        db.query(`INSERT INTO ALL_USER (Email, Password) VALUES(?,?)`,[email, hashedPassword],function(err, results){
+            //Registration failed
+            if(err){
+                console.log(err)
+                res.send({
+                    status: false,
+                    message: err.sqlMessage
+                })
         }
-        //Registration success
-        else {
-          console.log(results);
-          res.send({
-            status: true,
-            message: "Successfully registered",
-          });
-        }
-      }
-    );
-  },
-
-  //Logout USERS
-  logout: async (req, res) => {
+            //Registration success  
+            else  {
+              console.log(results)
+              res.send({
+                  status: true,
+                  message:"Successfully registered"
+              })
+            }
+          })
+      },
+  
+      //Logout USERS
+      logout : async (req, res) => {
+  
     //Acquiring data from body
     const email = req.body.email; /*2019ugcs001@nitjsr.ac.in*/
     const password = req.body.password; /*'password'*/
@@ -182,26 +174,24 @@ module.exports = {
               });
             } else {
 
-              // Deleting the jwt token from database
-              db.query(
-                `UPDATE ALL_USER SET Token = NULL WHERE Email = ?`,
-                [email],
-                function (err, results) {
-                  if (err) {
-                    res.send({
-                      status: false,
-                      message: err.sqlMessage,
-                    });
-                  } else {
-                    res.send({
-                      status: true,
-                      message: "Successfully logged you out",
-                    });
-                  }
-                }
-              );
+        // Deleting the jwt token from database
+        db.query(`UPDATE ALL_USER SET Token = NULL WHERE Email = ?`,[req.body.email],function(err, results){
+          if(err){
+              res.send({
+                  status: false,
+                  message: err.sqlMessage
+              })
             }
-          } else {
+            else{
+                res.send({
+                    status: true,
+                    message:"Successfully logged you out"
+                })
+                  }
+                })
+            }
+          } 
+          else {
             console.log("User not found");
             res.status(400).send({
               status:false,
